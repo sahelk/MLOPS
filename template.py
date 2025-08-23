c5ceb96131e0e68e5d5a1104324a770ad30fb899
sahelk <sahel.keyvan@gmail.com>
2025-08-23 23:15:16 +0200

    folder structure added

---
+++ template.py
@@ -0,0 +1,54 @@
+import os
+from pathlib import Path
+
+project_name = 'us_visa'
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py"
+    'app.py',
+    'requirements.txt',
+    'demo.py',
+    'setup.py',
+    'Dockerfile',
+    '.dockerignore',
+    'config/model.yaml',
+    'config/schema.yaml'
+
+]
+
+
+for file_path in list_of_files:
+    # make it os independent
+    file_path = Path(file_path)
+
+    file_dir, file_name = os.path.split(file_path)
+
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+
+    # create the file if it didn't exist or if it was empty
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, 'w') as f:
+            pass
+
+    else:
+        print(f'file is already present at {file_path}')
